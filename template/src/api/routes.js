ac4da80d047dd2b8f41cc763a3239322410bdd95
Omid A <omid.adibi@polymtl.ca>
2022-12-09 10:05:12 -0500

    assignment finished

--- template/src/api/routes.js
+++ template/src/api/routes.js
@@ -8,20 +8,44 @@ const { {{ allOperationIds }} } = require('./services/{{ channelName | kebabCase
 {%- endfor %}
 const router = Router();
 module.exports = router;
+
+
+let connections=[];
+let connectionIds=[];
+
 {% for channelName, channel in asyncapi.channels() -%}
 router.ws('{{channelName | pathResolve}}', async (ws, req) => {
+  ws.id = req.headers['sec-websocket-key'];
+  connectionIds.push(ws.id)
+  connections.push(ws)
   const path = pathParser(req.path);
-  console.log(`${yellow(path)} client connected.`);
-  {%- if channel.hasSubscribe() %}
-  await {{ channel.subscribe().id() }}(ws);
-  {%- endif %}
+  console.log(`${yellow(path)} client connected.${ws.id}`);
+  connections.forEach( async con=>{
+    {%- if channel.hasSubscribe() %}
+    await {{ channel.subscribe().id() }}(con,JSON.stringify({"connections":connectionIds}));
+    {%- endif %}
+  })
 
   {%- if channel.hasPublish() %}
   ws.on('message', async (msg) => {
-    console.log(`${yellow(path)} message was received:`);
-    console.log(util.inspect(msg, { depth: null, colors: true }));
-    await {{ channel.publish().id() }}(ws, { message: msg, path, query: req.query });
+    connections.forEach( async con=>{
+      if(con.id!==ws.id){
+        await {{ channel.publish().id() }}(con, { message: msg, path, query: req.query });
+        console.log(util.inspect(msg, { depth: null, colors: true }));
+      }
+    })
   });
+  {%- endif %}
+
+  {%- if channel.hasPublish() %}
+  ws.on('close',async () => {
+    connections=connections.filter((con)=>con.id!==ws.id)
+    connectionIds=connectionIds.filter((con)=>con!==ws.id)
+    connections.forEach( async con=>{
+      await {{ channel.subscribe().id() }}(con,JSON.stringify({"disconnections":ws?.id}));
+      console.log(util.inspect(JSON.stringify({"disconnection":ws.id}), { depth: null, colors: true }));
+    })
+  })
   {%- endif %}
 });
 {% endfor -%}
--- template/src/api/services/$$channel$$.js
+++ template/src/api/services/$$channel$$.js
@@ -3,9 +3,10 @@ const service = module.exports = {};
 /**
  * {{ channel.subscribe().summary() }}
  * @param {object} ws WebSocket connection.
+ * @param {string} msg The message that was received.
  */
-service.{{ channel.subscribe().id() }} = async (ws) => {
-  ws.send('Message from the server: Implement here your business logic that sends messages to a client after it connects.');
+service.{{ channel.subscribe().id() }} = async (ws,msg) => {
+  ws.send(msg);
 };
 
 {%- endif %}
@@ -14,8 +15,6 @@ service.{{ channel.subscribe().id() }} = async (ws) => {
  * {{ channel.publish().summary() }}
  * @param {object} ws WebSocket connection.
  * @param {object} options
- * @param {string} options.path The path in which the message was received.
- * @param {object} options.query The query parameters used when connecting to the server.
  * @param {%raw%}{{%endraw%}{{channel.publish().message(0).payload().type()}}{%raw%}}{%endraw%} options.message The received message.
 {%- if channel.publish().message(0).headers() %}
 {%- for fieldName, field in channel.publish().message(0).headers().properties() %}
@@ -28,8 +27,10 @@ service.{{ channel.subscribe().id() }} = async (ws) => {
 {%- endfor %}
 {%- endif %}
  */
-service.{{ channel.publish().id() }} = async (ws, { message, path, query }) => {
-  ws.send('Message from the server: Implement here your business logic that reacts on messages sent from a client.');
+service.{{ channel.publish().id() }} = async (ws, { message}) => {
+  if(message){
+    ws.send(message);
+  }
 };
 
 {%- endif %}
